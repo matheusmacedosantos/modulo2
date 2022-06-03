87f540da41dcf128c9a5c33bcd9b9aad18fbbadc
Matheus Macedo Santos <matheus.santos@sou.inteli.edu.br>
2022-06-03 16:54:28 -0300

    comecei as atividadesObrigatorias

---
+++ 04_AUT_EST_EX_OBRIGATORIOS/Semana 7/script.js
@@ -0,0 +1,10 @@
+var n1 = document.getElementById("n1");
+var resultado1 = document.getElementById("resultado1");
+
+function somar() {
+   resultado1.innerHTML = parseInt(n1.value) + 1;
+}
+
+function subtrair() {
+    resultado1.innerHTML = parseInt(n1.value) - 1;
+ }
