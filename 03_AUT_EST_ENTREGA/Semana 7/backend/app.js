6f341cf9d36881d4a20c98a184279515ed09ad87
Matheus Macedo Santos <matheus.santos@sou.inteli.edu.br>
2022-06-03 17:31:21 -0300

    cruds

---
+++ 03_AUT_EST_ENTREGA/Semana 7/backend/app.js
@@ -0,0 +1,90 @@
+const express = require('express'); 
+const bodyParser = require('body-parser');
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
+const hostname = '127.0.0.1';
+
+const port = 3061;
+const sqlite3 = require('sqlite3').verbose();
+const app = express();
+const DBPATH = '../db/database.db';
+
+app.use(express.static("../"));
+
+app.use(express.json());
+
+
+/* Definição dos endpoints */
+
+/****** CRUD ******************************************************************/
+
+// Retorna todos registros (é o R do CRUD - Read)
+app.get('/users', (req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Access-Control-Allow-Origin', '*'); // Isso é importante para evitar o erro de CORS
+
+	var db = new sqlite3.Database(DBPATH); // Abre o banco
+  var sql = 'SELECT * FROM users';
+	db.all(sql, [],  (err, rows ) => {
+		if (err) {
+		    throw err;
+		}
+		res.json(rows);
+	});
+	db.close(); // Fecha o banco
+});
+
+// NETWORK - inserir/criar
+app.post('/userinsert', urlencodedParser, (req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	// insere valores de nome e tipo segundo a request enviada pelo cliente
+	sql = "INSERT INTO tbUser (name, ocupation, age) VALUES ('" + req.body.name + "', '" + req.body.ocupation + "', '" + req.body.age + "')";
+	var db = new sqlite3.Database(DBPATH);
+	db.run(sql, [],  err => {
+		if (err) {
+		    throw err;
+		}
+	});
+	db.close();
+	res.end();
+});
+
+// Atualiza um registro (é o U do CRUD - Update)
+app.post('/userupdate', urlencodedParser, (req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Isso é importante para evitar o erro de CORS
+
+    sql = "UPDATE tbUser SET name = '" + req.body.name + "' WHERE age = " + req.body.age;
+    var db = new sqlite3.Database(DBPATH); // Abre o banco
+    db.run(sql, [],  err => {
+        if (err) {
+            throw err;
+        }
+        res.end();
+    });
+    db.close(); // Fecha o banco
+});
+
+// Exclui um registro (é o D do CRUD - Delete)
+app.post('/userdelete', urlencodedParser, (req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Isso é importante para evitar o erro de CORS
+
+    sql = "DELETE FROM tbUser WHERE age = " + req.body.age;
+    var db = new sqlite3.Database(DBPATH); // Abre o banco
+    db.run(sql, [],  err => {
+        if (err) {
+            throw err;
+        }
+        res.end();
+    });
+    db.close(); // Fecha o banco
+});
+
+
+
+
+/* Inicia o servidor */
+app.listen(port, hostname, () => {
+  console.log(`BD server running at http://${hostname}:${port}/`);
+});
